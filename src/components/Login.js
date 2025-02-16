4f11cffac0d814f3e4e5e29372225e1ae0f6c982
VTU9730 <prathapsiddavatam@gmail.com>
2025-02-16 11:51:00 +0530

    form-validation-added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,24 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import {checkValidData} from '../utils/validate'
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true)
+  const [error, setError] = useState(null)
+
+  const emailRef = useRef(null)
+  const passwordRef= useRef(null)
+  const nameRef = useRef(null)
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm)
   }
+
+  const handleButtonClick = () => {
+      const message = checkValidData(emailRef.current.value, passwordRef.current.value, nameRef.current.value)
+      setError(message)
+  }
+
   return (
     <div>
       <Header />
@@ -17,27 +29,31 @@ const Login = () => {
         />
       </div>
 
-      <form className="relative py-2 px-3 z-20 w-3/12 h-100 bg-black my-20 mx-auto rounded-lg text-white bg-opacity-80">
+      <form onSubmit={(e)=>e.preventDefault()} className="relative py-2 px-3 z-20 w-3/12 h-100 bg-black my-20 mx-auto rounded-lg text-white bg-opacity-80">
         <h1 className=" text-white float-start px-2 mt-4">{isSignInForm?"Sign In":"Sign Up"}</h1>
         {!isSignInForm && <input
-          type="email"
-          placeholder="Enter mail"
+          ref={nameRef}
+          type="text"
+          placeholder="Enter Full Name"
           className=" border-solid outline outline-1 outline-gray-600 mt-6 bg-gray-800 p-2 w-full "
         />}
         
         <input
+          ref={emailRef}
           type="email"
           placeholder="Enter mail"
           className=" border-solid outline outline-1 outline-gray-600 mt-6 bg-gray-800 p-2 w-full "
         />
         <br />
         <input
+          ref={passwordRef}
           type="password"
           placeholder="Enter password"
           className=" border-solid outline outline-1 outline-gray-600 mt-4 bg-gray-800 p-2 w-full "
         />
         <br />
-        <button className=" text-white bg-red-600 px-12 py-1.5  w-full mt-6 rounded-md">
+        <p className="text-red-600 py-4">{error}</p>
+        <button onClick={handleButtonClick} className=" text-white bg-red-600 px-12 py-1.5  w-full mt-6 rounded-md">
           {isSignInForm?"Sign In":"Sign Up"}
         </button>
         <p className="my-4 cursor-pointer" onClick={toggleSignInForm}>{isSignInForm?"New to Netflix? Sign Up Now":"Already Registered. Sign In Now"}</p>
---
+++ src/utils/validate.js
@@ -0,0 +1,9 @@
+export const checkValidData = (email,password,name) => {
+    const isValidEmail = /^[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/.test(email)
+    const isValidPassword = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/.test(password)
+    const isValidName = /^[a-zA-Z]+ [a-zA-Z]+$/.test(name)
+    if(!isValidEmail) return "Enter a valid Email"
+    if(!isValidPassword) return "Enter a valid Password"
+    if(!isValidName) return "Enter a valid Name"
+    return null
+}
